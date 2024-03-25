d01beaa652f0d6e2caf9dc646ee00ecb18d6595c
aravindkrishna2008 <86750995+aravindkrishna2008@users.noreply.github.com>
2024-03-25 07:57:45 -0700

    juntitiititit tests

--- JUSafeTradeTest.java
+++ JUSafeTradeTest.java
@@ -10,204 +10,370 @@ import static org.junit.Assert.*;
 import junit.framework.JUnit4TestAdapter;
 
 /**
- * SafeTrade tests:
- *   TradeOrder
- *   PriceComparator
- *   Trader
- *   Brokerage
- *   StockExchange
- *   Stock
+ * SafeTrade tests: TradeOrder PriceComparator Trader Brokerage StockExchange
+ * Stock
  *
  * @author TODO Name of principal author
  * @author TODO Name of group member
  * @author TODO Name of group member
  * @version TODO date
  * @author Assignment: JM Chapter 19 - SafeTrade
- * 
  * @author Sources: TODO sources
- *
  */
 public class JUSafeTradeTest
 {
     // --Test TradeOrder
     /**
-     * TradeOrder tests:
-     *   TradeOrderConstructor - constructs TradeOrder and then compare toString
-     *   TradeOrderGetTrader - compares value returned to constructed value
-     *   TradeOrderGetSymbol - compares value returned to constructed value
-     *   TradeOrderIsBuy - compares value returned to constructed value
-     *   TradeOrderIsSell - compares value returned to constructed value
-     *   TradeOrderIsMarket - compares value returned to constructed value
-     *   TradeOrderIsLimit - compares value returned to constructed value
-     *   TradeOrderGetShares - compares value returned to constructed value
-     *   TradeOrderGetPrice - compares value returned to constructed value
-     *   TradeOrderSubtractShares - subtracts known value & compares result
-     *     returned by getShares to expected value
+     * TradeOrder tests: TradeOrderConstructor - constructs TradeOrder and then
+     * compare toString TradeOrderGetTrader - compares value returned to
+     * constructed value TradeOrderGetSymbol - compares value returned to
+     * constructed value TradeOrderIsBuy - compares value returned to
+     * constructed value TradeOrderIsSell - compares value returned to
+     * constructed value TradeOrderIsMarket - compares value returned to
+     * constructed value TradeOrderIsLimit - compares value returned to
+     * constructed value TradeOrderGetShares - compares value returned to
+     * constructed value TradeOrderGetPrice - compares value returned to
+     * constructed value TradeOrderSubtractShares - subtracts known value &
+     * compares result returned by getShares to expected value
      */
-    private String symbol = "GGGL";
-    private boolean buyOrder = true;
-    private boolean marketOrder = true;
-    private int numShares = 123;
-    private int numToSubtract = 24;
-    private double price = 123.45;
+    private String  symbol        = "GGGL";
+    private boolean buyOrder      = true;
+    private boolean marketOrder   = true;
+    private int     numShares     = 123;
+    private int     numToSubtract = 24;
+    private double  price         = 123.45;
 
     // --Test Trader
     @Test
     public void traderConstructor()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
+        Trader trader = new Trader(null, "goodName", "pass");
 
-        assertEquals( "<< Invalid Trader constructed - screenName >>",
-                      trader.getName(), "goodName" );
-        assertEquals( "<< Invalid Trader constructed  - password >>",
-                      trader.getPassword(), "pass" );
-        assertNotNull( "<< Invalid Trader constructed - mailbox >>",
-                       trader.mailbox() );
+        assertEquals("<< Invalid Trader constructed - screenName >>", trader.getName(), "goodName");
+        assertEquals("<< Invalid Trader constructed  - password >>", trader.getPassword(), "pass");
+        assertNotNull("<< Invalid Trader constructed - mailbox >>", trader.mailbox());
     }
+
 
     @Test
     public void traderGetName()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        assertEquals( "<< Trader.getName invalid screenName >>", "goodName",
-                      trader.getName() );
+        Trader trader = new Trader(null, "goodName", "pass");
+        assertEquals("<< Trader.getName invalid screenName >>", "goodName", trader.getName());
     }
+
 
     @Test
     public void traderGetPassword()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        assertEquals( "<< Trader.getPassword invalid password >>", "pass",
-                      trader.getPassword() );
+        Trader trader = new Trader(null, "goodName", "pass");
+        assertEquals("<< Trader.getPassword invalid password >>", "pass", trader.getPassword());
     }
+
 
     @Test
     public void traderCompareTo()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        Trader traderGreater = new Trader( null, "z_goodName", "greater" );
-        Trader traderLess = new Trader( null, "a_goodName", "less" );
-        Trader traderEqual = new Trader( null, "goodName", "equal" );
-        assertTrue( "<< Trader.compareTo > fails >>",
-                    traderGreater.compareTo( trader ) > 0 );
-        assertTrue( "<< Trader.compareTo < fails >>",
-                    traderLess.compareTo( trader ) < 0 );
-        assertTrue( "<< Trader.compareTo == fails >>",
-                    traderEqual.compareTo( trader ) == 0 );
+        Trader trader = new Trader(null, "goodName", "pass");
+        Trader traderGreater = new Trader(null, "z_goodName", "greater");
+        Trader traderLess = new Trader(null, "a_goodName", "less");
+        Trader traderEqual = new Trader(null, "goodName", "equal");
+        assertTrue("<< Trader.compareTo > fails >>", traderGreater.compareTo(trader) > 0);
+        assertTrue("<< Trader.compareTo < fails >>", traderLess.compareTo(trader) < 0);
+        assertTrue("<< Trader.compareTo == fails >>", traderEqual.compareTo(trader) == 0);
     }
+
 
     @Test
     public void traderEquals()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        Trader traderEqual = new Trader( null, "goodName", "equal" );
-        assertTrue( "<< Trader.equals fails >>", traderEqual.equals( trader ) );
+        Trader trader = new Trader(null, "goodName", "pass");
+        Trader traderEqual = new Trader(null, "goodName", "equal");
+        assertTrue("<< Trader.equals fails >>", traderEqual.equals(trader));
     }
+
 
     @Test
     public void traderNotEquals()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        Trader traderEqual = new Trader( null, "badName", "equal" );
-        assertFalse( "<< Trader.equals fails >>", traderEqual.equals( trader ) );
+        Trader trader = new Trader(null, "goodName", "pass");
+        Trader traderEqual = new Trader(null, "badName", "equal");
+        assertFalse("<< Trader.equals fails >>", traderEqual.equals(trader));
     }
+
 
     @Test
     public void traderNotEqualsNull()
     {
         Trader trader = null;
-        Trader traderEqual = new Trader( null, "badName", "equal" );
-        assertFalse( "<< Trader.equals fails >>", traderEqual.equals( trader ) );
+        Trader traderEqual = new Trader(null, "badName", "equal");
+        assertFalse("<< Trader.equals fails >>", traderEqual.equals(trader));
     }
+
 
     @Test
     public void traderNotEqualsNotTrader()
     {
         String trader = "not a trader";
-        Trader traderEqual = new Trader( null, "badName", "equal" );
-        assertFalse( "<< Trader.equals fails >>", traderEqual.equals( trader ) );
+        Trader traderEqual = new Trader(null, "badName", "equal");
+        assertFalse("<< Trader.equals fails >>", traderEqual.equals(trader));
     }
+
 
     @Test
     public void traderHasMessages()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        assertFalse( "<< Trader.hasMessages failed >>", trader.hasMessages() );
-        trader.receiveMessage( "zyzzx" );
-        assertTrue( "<< Trader.hasMessages failed >>", trader.hasMessages() );
+        Trader trader = new Trader(null, "goodName", "pass");
+        assertFalse("<< Trader.hasMessages failed >>", trader.hasMessages());
+        trader.receiveMessage("zyzzx");
+        assertTrue("<< Trader.hasMessages failed >>", trader.hasMessages());
     }
+
 
     @Test
     public void traderReceiveMessage()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
+        Trader trader = new Trader(null, "goodName", "pass");
 
-        trader.receiveMessage( "zyzzx" );
-        assertTrue( "<< Trader.receiveMessage- hasMessages failed >>",
-                    trader.hasMessages() );
+        trader.receiveMessage("zyzzx");
+        assertTrue("<< Trader.receiveMessage- hasMessages failed >>", trader.hasMessages());
 
         Queue<String> mbox = trader.mailbox();
 
-        //System.err.println(mbox.peek());
-        assertEquals( mbox.peek(), "zyzzx" );
+        // System.err.println(mbox.peek());
+        assertEquals(mbox.peek(), "zyzzx");
 
         trader.openWindow(); // called for code coverage. Should empty
                              // mailbox
 
-        assertTrue( "<< Trader.openWindow - hasMessages failed >>",
-                    !trader.hasMessages() );
+        assertTrue("<< Trader.openWindow - hasMessages failed >>", !trader.hasMessages());
     }
+
 
     @Test
     public void traderGetQuote()
     {
         StockExchange safe = new StockExchange();
-        safe.listStock( "ABCD", "wxyz", 123.45 );
+        safe.listStock("ABCD", "wxyz", 123.45);
 
-        Brokerage broke = new Brokerage( safe );
-        Trader trader = new Trader( broke, "goodName", "pass" );
-        trader.getQuote( "ABCD" );
+        Brokerage broke = new Brokerage(safe);
+        Trader trader = new Trader(broke, "goodName", "pass");
+        trader.getQuote("ABCD");
 
         // System.out.println(trader);
-        assertTrue( "<<< Trader.getQuote failed >>>", trader.hasMessages() );
+        assertTrue("<<< Trader.getQuote failed >>>", trader.hasMessages());
 
         Queue<String> mbox = trader.mailbox();
         // System.err.println(mbox);
-        assertTrue( "<<< Trader.getQuote - invalid quote >>>", mbox.peek()
-            .contains( "123.45" ) );
+        assertTrue("<<< Trader.getQuote - invalid quote >>>", mbox.peek().contains("123.45"));
 
         trader.quit();
     }
+
 
     @Test
     public void traderPlaceOrder()
     {
         StockExchange safe = new StockExchange();
-        safe.listStock( "ABCD", "wxyz", 123.45 );
+        safe.listStock("ABCD", "wxyz", 123.45);
 
-        Brokerage broke = new Brokerage( safe );
+        Brokerage broke = new Brokerage(safe);
 
-        Trader trader = new Trader( broke, "goodName", "pass" );
+        Trader trader = new Trader(broke, "goodName", "pass");
         Queue<String> mbox = trader.mailbox();
-        TradeOrder order = new TradeOrder( trader, "ABC", true, true, 100,
-            123.99 );
-        trader.placeOrder( order );
+        TradeOrder order = new TradeOrder(trader, "ABC", true, true, 100, 123.99);
+        trader.placeOrder(order);
 
-        assertTrue( "<<< Trader.placeOrder - invalid message >>>",
-                    mbox.remove().contains( "ABC not found" ) );
+        assertTrue(
+            "<<< Trader.placeOrder - invalid message >>>",
+            mbox.remove().contains("ABC not found"));
 
-        order = new TradeOrder( trader, "ABCD", true, true, 100, 123.99 );
-        safe.placeOrder( order );
+        order = new TradeOrder(trader, "ABCD", true, true, 100, 123.99);
+        safe.placeOrder(order);
         // System.err.println(mbox);
-        assertTrue( "<<< Trader.placeOrder - invalid message >>>", mbox.peek()
-            .contains( "ABCD" ) );
+        assertTrue("<<< Trader.placeOrder - invalid message >>>", mbox.peek().contains("ABCD"));
     }
+
 
     @Test
     public void TraderToString()
     {
-        Trader trader = new Trader( null, "goodName", "pass" );
-        assertNotNull( trader.toString() );
+        Trader trader = new Trader(null, "goodName", "pass");
+        assertNotNull(trader.toString());
     }
-}
 
+
+    @Test
+    public void StockGetQuote()
+    {
+        Stock stock = new Stock("i am cool", "sdfd", 1);
+        assertNotNull(stock.getQuote());
+    }
+
+
+    @Test
+    public void StockConstructor()
+    {
+        Stock stock = new Stock("i am cool", "sdfd", 1);
+        assertNotNull(stock);
+    }
+
+
+    @Test
+    public void StockGetQuoteString()
+    {
+        Stock stock = new Stock("i am cool", "sdfd", 1);
+        assertNotNull(stock.getQuote());
+    }
+
+
+    @Test
+    public void StockPlaceOrder()
+    {
+        Stock stock = new Stock("i am cool", "sdfd", 1);
+        TradeOrder order = new TradeOrder(null, "GGGL", true, false, 1, 1);
+        stock.placeOrder(order);
+    }
+
+
+    @Test
+    public void StockExchangeConstructor()
+    {
+        StockExchange stock = new StockExchange();
+        assertNotNull(stock);
+    }
+
+
+    @Test
+    public void PriceComparatorConstructor()
+    {
+        PriceComparator pc = new PriceComparator(true);
+        assertNotNull(pc);
+    }
+
+
+    @Test
+    public void PriceComparatorCompare()
+    {
+        PriceComparator pc = new PriceComparator(true);
+        TradeOrder to1 = new TradeOrder(null, "GGGL", true, false, 123, 10);
+        TradeOrder to2 = new TradeOrder(null, "GGGL", true, false, 123, 10);
+        pc.compare(to1, to2);
+    }
+
+
+    @Test
+    public void BrokerageTestContrsutrcotr()
+    {
+        StockExchange stock = new StockExchange();
+        Brokerage b = new Brokerage(stock);
+        assertNotNull(b);
+    }
+
+
+    @Test
+    public void BrokerageAddUser()
+    {
+        StockExchange stock = new StockExchange();
+        Brokerage b = new Brokerage(stock);
+        b.addUser("aravind", "shreyas");
+    }
+
+
+    @Test
+    public void BrokerageLogin()
+    {
+        StockExchange stock = new StockExchange();
+        Brokerage b = new Brokerage(stock);
+        b.addUser("aravind", "shreyas");
+        b.login("aravind", "shreyas");
+    }
+
+
+    @Test
+    public void BrokerageLogout()
+    {
+        StockExchange stock = new StockExchange();
+        Brokerage b = new Brokerage(stock);
+        b.addUser("aravind", "shreyas");
+        b.login("aravind", "shreyas");
+        b.logout(b.getTraders().get("aravind"));
+    }
+
+    // brokerage getQuote
+    // brokerage placeOrder
+
+
+    @Test
+    public void TraderOrderGetTrader()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertEquals(order.getTrader(), new Trader(null, "shreyas", "aravind"));
+    }
+
+
+    @Test
+    public void TraderOrderGetSymbol()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertEquals(order.getSymbol(), "GGGL");
+    }
+
+
+    @Test
+    public void TraderOrderIsBuy()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertTrue(order.isBuy());
+    }
+
+
+    @Test
+    public void TraderOrderIsSell()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertFalse(order.isSell());
+    }
+
+
+    @Test
+    public void TraderOrderIsMarket()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertFalse(order.isMarket());
+    }
+
+
+    @Test
+    public void TraderOrderIsLimit()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertTrue(order.isLimit());
+    }
+
+
+    @Test
+    public void TraderOrderGetShares()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertEquals(order.getShares(), 1);
+    }
+
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void TraderOrderGetPrice()
+    {
+        TradeOrder order =
+            new TradeOrder(new Trader(null, "shreyas", "aravind"), "GGGL", true, false, 1, 1);
+        assertEquals(order.getPrice(), 1.0, 0.005);
+    }
+
+}
